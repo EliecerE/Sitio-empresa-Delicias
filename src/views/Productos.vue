6cf8cf0f4734c661a25b5555b2b2bc5ac04a82f1
Eliecer Escudero <eliecerescudero687@gmail.com>
2023-11-22 21:55:45 -0600

    EFE

--- src/views/Productos.vue
+++ src/views/Productos.vue
@@ -1,16 +1,12 @@
+<
 <template>
-	<div>
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, cumque! Magni optio quaerat expedita accusantium non
-			tempore sunt est aut commodi. Omnis illo fugiat quas temporibus architecto officiis at facere!
-		</p>
-	</div>
+	<div><h1>Productos</h1></div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
 	body{
-	background-color: rgb(158, 47, 47);
+	  background-color: rebeccapurple;
 	}
 </style>
