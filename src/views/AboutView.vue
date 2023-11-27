b18b5c6acb3d6fa36f29c2bb21d3617d8e507d55
Eliecer Escudero <eliecerescudero687@gmail.com>
2023-11-26 21:46:36 -0600

    sdde

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -16,8 +16,20 @@
 				src="@/assets/logo_empresa.jpeg"
 				alt="" />
 		</div>
+
+		<mi-footer></mi-footer>
 	</main>
 </template>
+
+<script>
+	import footer from '../components/Footer.vue';
+	export default {
+		components: {
+			 // Registra el componente MiComponente para su uso en este componente
+			 'mi-footer': footer
+		 }
+		}
+</script>
 
 <style scoped>
 		.hero{
--- src/views/Contacto.vue
+++ src/views/Contacto.vue
@@ -16,27 +16,96 @@
 						alt="imagen Usuario" />
 				</div>
 				<div class="usuarioText">
-					<h3>Eliecer Escudero - <span> Jefe principal</span></h3>
-					<h3>Email <a href="mailto:elicerescudero687@gmail.com">elicerescudero687@gmail.com</a></h3>
 					<h3>
-						Telefono y Whatsapp: <a href="tel:+50769297800">69297800</a> <font-awesome-icon :icon="['fad', 'phone']" />
+						Eliecer Escudero -
+						<span
+							>Jefe principal
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="1em"
+								viewBox="0 0 448 512">
+								<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+								<path
+									d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+							</svg>
+						</span>
 					</h3>
-
-					<font-awesome-icon :icon="['fad', 'phone']" />
+					<h3>
+						Email
+						<a href="mailto:elicerescudero687@gmail.com"
+							>elicerescudero687@gmail.com<svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="1em"
+								viewBox="0 0 512 512">
+								<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+								<path
+									d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" /></svg
+						></a>
+					</h3>
+					<h3>
+						Telefono y Whatsapp:
+						<a href="tel:+50769297800"
+							>69297800<svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="1em"
+								viewBox="0 0 512 512">
+								<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+								<path
+									d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" /></svg
+						></a>
+					</h3>
 				</div>
 			</div>
-
-			<div class="usuario">
+			<div class="usuario usuario2">
 				<div class="imagenUsuario">
 					<img
-						src="/UbicacionEmpres/persona1.jpg"
+						src="/UbicacionEmpres/persona2.avif"
 						alt="imagen Usuario" />
 				</div>
-				<h3>Eliecer Escudero - Jefe principal</h3>
-				<h3>Email <a href="mailto:elicerescudero687@gmail.com">elicerescudero687@gmail.com</a></h3>
-				<h3>Whatsapp: <a href="tel:+50769297800">69297800</a></h3>
+				<div class="usuarioText">
+					<h3>
+						Katyusca Ortega -
+						<span
+							>Gerente General
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="1em"
+								viewBox="0 0 448 512">
+								<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+								<path
+									d="M304 128a80 80 0 1 0 -160 0 80 80 0 1 0 160 0zM96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM49.3 464H398.7c-8.9-63.3-63.3-112-129-112H178.3c-65.7 0-120.1 48.7-129 112zM0 482.3C0 383.8 79.8 304 178.3 304h91.4C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7H29.7C13.3 512 0 498.7 0 482.3z" />
+							</svg>
+						</span>
+					</h3>
+					<h3>
+						Email
+						<a href="mailto:katiuscao1929@gmail.com"
+							>katiuscao1929@gmail.com<svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="1em"
+								viewBox="0 0 512 512">
+								<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+								<path
+									d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" /></svg
+						></a>
+					</h3>
+					<h3>
+						Telefono y Whatsapp:
+						<a href="tel:+50762893473"
+							>62893473<svg
+								xmlns="http://www.w3.org/2000/svg"
+								height="1em"
+								viewBox="0 0 512 512">
+								<!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+								<path
+									d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z" /></svg
+						></a>
+					</h3>
+				</div>
 			</div>
 		</div>
+
+		<mi-formulario></mi-formulario>
 
 		<h2 class="titulo">En dónde estamos?</h2>
 		<div class="ubicacionEmpresa">
@@ -63,84 +132,98 @@
 </template>
 
 <script>
-		import footer from '../components/Footer.vue';
+				import footer from '../components/Footer.vue';
+	import formulario from '../components/Formulario.vue';
 
-	export default {
-		components: {
-			 // Registra el componente MiComponente para su uso en este componente
-			 'mi-footer': footer
-		 }
-	}
+			export default {
+				components: {
+					 // Registra el componente MiComponente para su uso en este componente
+					 'mi-footer': footer,
+					 'mi-formulario': formulario
+				 }
+			}
 </script>
 
 <style lang="scss" scoped>
 
-			h3{
-				font-family:system-ui, -apple-system;
-				font-size: 1.3rem;
-			}
-					.GeneratedMarquee{
-						font-size: 3rem;
-						background-color: firebrick;
-						color: #fff;
+	svg{
+		margin-left: .2rem;
+		display: inline-block;
+		padding: .5rem;
+		background-color: chocolate;
+		border-radius: 5px;
+	}
+
+				h3{
+					font-family:system-ui, -apple-system;
+					font-size: 1.3rem;
+				}
+						.GeneratedMarquee{
+							font-size: 2.7rem;
+							background-color: firebrick;
+							color: #fff;
+						}
+
+
+					.boxUsuarios{
+						height: 70vh;
+			display: flex;
+			flex-flow: row wrap;
+			justify-content: center;
+			align-items: center;
+			gap: 0 6rem;
 					}
 
+		.usuario{
+			background-color: rgb(233, 214, 233);
+			padding: 2rem;
+			border-radius: 3px;
+			box-shadow: 0 0 4px rgb(67, 67, 183);
+			display: flex;
+			flex-flow: column wrap;
+			justify-content: space-between;
+			align-items: center;
 
-				.boxUsuarios{
-					height: 70vh;
-		display: flex;
-		flex-flow: row wrap;
-		justify-content: center;
-		align-items: center;
-		gap: 0 6rem;
-				}
-
-	.usuario{
-		background-color: rgb(233, 214, 233);
-		padding: 2rem;
-		border-radius: 3px;
-		box-shadow: 0 0 4px rgb(67, 67, 183);
-		display: flex;
-		flex-flow: column wrap;
-		justify-content: space-between;
-		align-items: center;
-
-		span{
-			font-style: italic;
-			font-size: 1.2rem;
-			color: darkorange;
+			span{
+				font-style: italic;
+				font-size: 1.2rem;
+				color: darkorange;
+			}
 		}
-	}
 
-	.usuario1{
-				background-color: rgba(236, 180, 198, 0.384);
-	}
+		.usuario1{
+					background-color: rgba(236, 180, 198, 0.384);
+		}
+
+		.usuario2{
+			background-color: rgb(218, 245, 236);
+		}
 
 
-				.imagenUsuario{
-				width: 200px;
-				height: 200px;
-				img{
-					border-radius: 50%;
-				}
-				}
+					.imagenUsuario{
+					width: 200px;
+					height: 200px;
+					img{
+						border-radius: 50%;
+					}
+					}
 
-				.usuarioText{
-					margin-top: 2rem;
-					text-align: justify;
-				}
+					.usuarioText{
+						margin-top: 2rem;
+						text-align: justify;
+					}
 
-						.titulo{
-							padding: 0 2rem;
-							font-size: 4rem;
-							border-bottom: 3px solid gold;
-							margin-bottom: 2rem;
-							background-color: rgb(237, 206, 227);
-							color: rgb(40, 12, 125);
-						}
+							.titulo{
+								padding: 0 2rem;
+								font-size: 4rem;
+								border-bottom: 3px solid gold;
+								margin-bottom: 2rem;
+								background-color: rgb(237, 206, 227);
+								color: rgb(40, 12, 125);
+							}
 
-						.ubicacionEmpresa{
-							display: flex;
-							flex-flow: row wrap;
-						}
+							.ubicacionEmpresa{
+								display: flex;
+								flex-flow: row wrap;
+							}
 </style>
